e6eb3d22a3b9361f0fbea8cbbc18e5ce9ff132fd
Joseph <joseph.dena@student.moringaschool.com>
2024-02-13 09:04:54 +0300

    complete

---
+++ lib/migrations/versions/d1540a3362f1_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: d1540a3362f1
+Revises: 
+Create Date: 2024-02-13 08:16:34.212646
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd1540a3362f1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
